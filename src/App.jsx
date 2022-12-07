4d90b2aaea59d3cb3a0b3dbdcd5b12bf71bae1a0
davidyui <davidnsilvasoares@gmail.com>
2022-12-07 16:43:27 -0300

    Atualizando o Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./global.css"
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
         <div className="App">
           <Header/>
           <Outlet/>
+          <Footer/>
         </div>
       )  
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import instagram from "../images/instagram.png"
+import facebook from "../images/facebook.png"
+import twitter from "../images/twitter.png"
+import googleplus from "../images/google-plus.png"
+import "./Footer.css"
 export default function Footer () {
 
     return (
-        <>
-        
-        </>
+        <div className="Footer">
+        <h3>ReactFlix</h3>
+        <p> ReactFlix é um site de cinema feito para o projeto do curso Programadores Cariocas que têm como objetivo consumir fake API e utilizar os métodos HTTP</p>
+        <ul className="listaFooter">
+            <li><img className="imgFooter" src={instagram} alt="" /></li>
+            <li><img className="imgFooter" src={facebook} alt="" /> </li>
+            <li><img className="imgFooter" src={twitter} alt="" /> </li>
+            <li><img className="imgFooter" src={googleplus} alt="" /> </li>
+        </ul>
+        </div>
     )
 }
