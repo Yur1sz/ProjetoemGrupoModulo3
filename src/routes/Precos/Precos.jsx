5acef6140c8148ad80a3ac6497ebacf18d042f09
Priscila Barbosa <priscilabarbosa.fer@gmail.com>
2022-12-12 16:53:33 -0300

    Alterações na página Preço.

--- src/routes/Precos/Precos.jsx
+++ src/routes/Precos/Precos.jsx
@@ -1,5 +1,63 @@
+import "./Precos.css"
 export default function Precos() {
     return(
-        <h1>Preços</h1>
+        <div className="container-principal">
+            <div className="simples">
+                <div className="simples-titulo">
+                    <h2>Sala Simples</h2>
+                </div>
+                <div>
+                    <div>
+                        <ul>
+                            <li>Inteiro:</li>
+                            <li>Meia:</li>
+                        </ul>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+            <div className="premium">
+                <div className="premium-titulo">
+                    <h2>Sala Premium</h2>
+                </div>
+                <div>
+                    <div>
+                        <ul>
+                            <li>Inteiro:</li>
+                            <li>Meia:</li>
+                        </ul>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+            <div className="plus">
+                <div className="plus-titulo">
+                    <h2>React Plus</h2>
+                </div>
+                <div>
+                    <div>
+                        <ul>
+                            <li>Inteiro:</li>
+                            <li>Meia:</li>
+                        </ul>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+            <div className="vip">
+                <div className="vip-titulo">
+                    <h2>VIP</h2>
+                </div>
+                <div>
+                    <div>
+                        <ul>
+                            <li>Inteiro:</li>
+                            <li>Meia:</li>
+                        </ul>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     )
 }
