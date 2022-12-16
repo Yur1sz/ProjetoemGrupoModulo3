5f09f8cf9bf7f75dc6fe8f6a880bd3e154f188dc
GabeOP <gabrieloliveirap1@hotmail.com>
2022-12-15 23:36:36 -0300

    att geral CSS

--- src/routes/Cinemas/Cinemas.jsx
+++ src/routes/Cinemas/Cinemas.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import API from '../../axios/config'
+import "./Cinemas.css"
 export default function Cinemas() {
-  return(
-    <h1>Cinemas</h1>
-  )
+
+  const [cinema, setCinema] = useState([]);
+
+  useEffect(() => {
+    API.get("/cinemas").then((response) => setCinema(response.data));
+  }, []);
+
+  return (
+    <>
+      <h2>Conheça nossos cinemas</h2>
+      <ul className='lista-cinemas'>
+        <li>
+          {cinema.map(function locais(item) {
+            return <div>
+                    <p>Cinema {item.id}</p>
+                    <p><strong>Local:</strong> {item.local}</p>
+                    <p><strong>Estado:</strong> {item.estado}</p>
+                  </div>;
+          })}
+        </li>
+        
+      </ul>
+    </>
+  );
 }
--- src/routes/Precos/Precos.jsx
+++ src/routes/Precos/Precos.jsx
@@ -1,90 +1,33 @@
 import "./Precos.css"
+import { useEffect } from 'react';
+import { useState } from 'react';
+import API from '../../axios/config'
+import Cinemas from "../Cinemas/Cinemas";
+
 export default function Precos() {
-    return(
-        <div className="container-principal">
-            <div className="simples">
-                <div className="simples-titulo">
-                    <h2>Sala Simples</h2>
-                </div>
-                <div>
-                    <div>
-                        <ul>
-                            <li>Inteiro:</li>
-                            <li>Meia:</li>
-                        </ul>
-                    </div>
-                    <div></div>
-                </div>
-            </div>
-            <div className="premium">
-                <div className="premium-titulo">
-                    <h2>Sala Premium</h2>
-                </div>
-                <div>
-                    <div>
-                        <ul>
-                            <li>Inteiro:</li>
-                            <li>Meia:</li>
-                        </ul>
-                    </div>
-                    <div></div>
-                </div>
-            </div>
-            <div className="plus">
-                <div className="plus-titulo">
-                    <h2>React Plus</h2>
-                </div>
-                <div>
-                    <div>
-                        <ul>
-                            <li>Inteiro:</li>
-                            <li>Meia:</li>
-                        </ul>
-                    </div>
-                    <div></div>
-                </div>
-            </div>
-            <div className="vip">
-                <div className="vip-titulo">
-                    <h2>VIP</h2>
-                </div>
-                <div>
-                    <div>
-                        <ul>
-                            <li>Inteiro:</li>
-                            <li>Meia:</li>
-                        </ul>
-                    </div>
-                    <div></div>
-                </div>
-            </div>
-            <div className="react-basic">
-                <div className="reactbasic-titulo">
-                    <h2>React Basic</h2>
-                </div>
-                <div>
-                    <div>
-                        <ul>
-                            <li>Inteiro:</li>
-                            <li>Meia:</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div className="caverna">
-                <div className="caverna-titulo">
-                    <h2>Caverna</h2>
-                </div>
-                <div>
-                    <ul>
-                        <li>Inteiro:</li>
-                        <li>Meia:</li>
-                    </ul>
-                </div>
-            </div>
 
 
-        </div>
-    )
+    const [precos, setPreco] = useState([]);
+
+    useEffect(() => {
+        API.get("/preco").then((response) => setPreco(response.data));
+    }, []);
+
+
+    return (
+        <>
+        <h2>Preços</h2>
+        <ul className="lista-precos">
+            <li>
+                {precos.map(function valores(item){
+                    return <div>
+                        <p>Sala: {item.sala}</p>
+                        <p>Inteira: {item.valor}</p>
+                        <p>Meia: {item.valorMeia}</p>
+                    </div>
+                })}
+            </li>
+        </ul>
+     </>
+    );
 }
